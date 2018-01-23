db01eba63358253685cc0f157be2a67a880dee81
Michael Shelton <meshelton@google.com>
2018-01-23 18:36:17 -0500

    Add init.org file that generates init.el

--- init.el
+++ init.el
@@ -1,69 +1,14 @@
 ;;; .emacs --- Emacs initialization file -*- lexical-binding: t; -*-
 
-(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
-      (bootstrap-version 3))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; This file replaces itself with the actual configuration at first run.
 
-(straight-use-package 'use-package)
-
-(defun system-is-workstation ()
-  (interactive)
-  "Return true if the system we are running is my work desktop at Google"
-  (string-equal system-name "meshelton.nyc.corp.google.com")
-  )
-
-(if (system-is-workstation)
-    (require 'google)
-  (require 'p4-google)                ;; g4-annotate, improves find-file-at-point
-  (require 'compilation-colorization) ;; colorizes output of (i)grep
-  (require 'rotate-clients)           ;; google-rotate-client
-  (require 'rotate-among-files)       ;; google-rotate-among-files
-  (require 'googlemenu)               ;; handy Google menu bar
-  (require 'p4-files)                 ;; transparent support for Perforce filesystem
-  (require 'google3-build)            ;; support for blaze builds
-  (require 'csearch)                  ;; Search the whole Google code base.
-  )
-
-;;;; Appearance
-(load-theme 'wombat 1)
-(global-linum-mode 0)
-(column-number-mode 1)
-(line-number-mode 1)
-(setq inhibit-startup-screen 1)
-(show-paren-mode 1)
-(tool-bar-mode -1)
-(setq-default indent-tabs-mode nil)
-(setq default-tab-width 2)
-(setq compilation-scroll-output 'first-error)
-
-;;;; visual-regexp
-(straight-use-package 'visual-regexp)
-(straight-use-package 'visual-regexp-steroids)
-(require 'visual-regexp-steroids)
-(define-key global-map (kbd "C-c C-r") 'vr/replace)
-(define-key global-map (kbd "C-c C-q") 'vr/query-replace)
-(define-key global-map (kbd "C-c C-m") 'vr/mc-mark)
-;;;; multiple-cursors
-(straight-use-package 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-^") 'mc/mark-all-like-this)
-
-;;;; Backup
-(setq
- backup-by-copying t      ; don't clobber symlinks
- backup-directory-alist
- '(("." . "~/.saves"))    ; don't litter my fs tree
- delete-old-versions t
- kept-new-versions 6
- kept-old-versions 2
- version-control t)       ; use versioned backups
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
+;; We can't tangle without org!
+(require 'org)
+;; Open the configuration
+(find-file (concat user-emacs-directory "init.org"))
+;; tangle it
+(org-babel-tangle)
+;; load it
+(load-file (concat user-emacs-directory "init.el"))
+;; finally byte-compile it
+(byte-compile-file (concat user-emacs-directory "init.el"))
